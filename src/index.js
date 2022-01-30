8c1c931c88cae93464090c958198c50948693de8
Alexander Bychyk <sanya55bychyk@gmail.com>
2022-01-30 16:43:37 +0300

    feat: all tests passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let vault = [];
+  let isCorrect = true;
+  if (str.length % 2 != 0) {
+    isCorrect = false;
+  } else {
+    for (let i = 0; i < str.length; i++) {
+      bracketsConfig.forEach(element => {
+        if (str[i] == element[0] & str[i] == element[1]) {
+          if (vault[vault.length-1] != str[i]) {
+            vault.push(str[i]);
+          } else vault.pop();
+        } else {
+          if (element[0] == str[i]) {
+            vault.push(str[i]);
+          } 
+          else if (element[1] == str[i]) {
+            if (vault[vault.length-1] == element[0]) vault.pop();
+          
+          }
+        }
+      });
+    }
+    isCorrect = vault.length == 0 ? true : false;
+  }
+  
+  return isCorrect;
 }
